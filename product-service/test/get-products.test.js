5d05315c51b18d3982ce351215cb65193e6524a4
christina asipenka <krystinabutko@gmail.com>
2024-06-22 16:31:31 +0200

    Task 3 (#2)
    * initial commit
    
    * headers adjustment
    
    * tests
    
    * tests
    
    * readme upd
    
    * readme upd
    
    * README upd
    
    * Export Lambda Function ARNs
    
    * mock data
    
    * tests adjustment
    
    * swagger
    
    * CORS

---
+++ product-service/test/get-products.test.js
@@ -0,0 +1,21 @@
+const getProducts = require('../product-service/get-products');
+
+describe('getProducts Lambda Function Test', () => {
+    it('should return correct response structure', async () => {
+        const event = {};
+        const result = await getProducts.handler(event);
+
+        expect(result).toBeDefined();
+        expect(result.statusCode).toBe(200);
+        expect(result.headers).toEqual({
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": 'GET,POST,OPTIONS',
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": "application/json"
+        });
+
+        const responseBody = JSON.parse(result.body);
+        expect(responseBody).toBeDefined();
+        expect(responseBody).toEqual(expect.any(Array));
+    });
+});
