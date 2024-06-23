e9b1028361ef1e7c91540b787f7675dfc98b503f
Christina Asipenka <krystinabutko@gmail.com>
2024-06-23 13:10:15 +0200

    create product & swagger doc

--- product-service/lib/product-service-stack.js
+++ product-service/lib/product-service-stack.js
@@ -1,6 +1,7 @@
 const { Stack, Duration, CfnOutput } = require('aws-cdk-lib');
 const lambda = require('aws-cdk-lib/aws-lambda');
 const apigateway = require('aws-cdk-lib/aws-apigateway');
+const dynamodb = require('aws-cdk-lib/aws-dynamodb');
 
 class ProductServiceStack extends Stack {
   /**
@@ -12,27 +13,57 @@ class ProductServiceStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const stage = 'dev';
+    const productsTableName = 'products';
+    const stocksTableName = 'stocks';
+
+    // Import the existing DynamoDB table
+    const productsTable = dynamodb.Table.fromTableName(this, 'ProductsTable', productsTableName);
 
     const getProductsList = new lambda.Function(this, 'getProductsList', {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset('product-service'),
       handler: 'get-products.handler',
+      environment: {
+        PRODUCTS_TABLE: productsTable.tableName,
+      },
     });
 
     const getProduct = new lambda.Function(this, 'getProduct', {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset('product-service'),
       handler: 'get-product.handler',
+      environment: {
+        PRODUCTS_TABLE: productsTable.tableName,
+      },
     });
 
-    const api = new apigateway.LambdaRestApi(this, 'ProductServiceApi', {
-      handler: getProductsList,
-      proxy: false,
+    const createProduct = new lambda.Function(this, 'createProduct', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('product-service'),
+      handler: 'create-product.handler',
+      environment: {
+        PRODUCTS_TABLE: productsTable.tableName,
+        STOCKS_TABLE: stocksTableName.tableName,
+      },
+    });
+
+    productsTable.grantReadWriteData(getProductsList);
+    productsTable.grantReadWriteData(getProduct);
+    productsTable.grantReadWriteData(createProduct);
+
+    const api = new apigateway.RestApi(this, 'ProductServiceApi', {
+      deployOptions: {
+        stageName: 'prod',
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: ['GET', 'POST', 'OPTIONS'],
+      },
     });
 
     const productsResource = api.root.addResource('products');
-    productsResource.addMethod('GET');
+    productsResource.addMethod('GET', new apigateway.LambdaIntegration(getProductsList));
+    productsResource.addMethod('POST', new apigateway.LambdaIntegration(createProduct));
 
     const productResource = api.root.addResource('product');
     const productIdResource = productResource.addResource('{id}');
